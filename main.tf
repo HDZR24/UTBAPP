8ac05c7aa9ae5a2c04445941f41b80392cffd0cc
HDZR24 <hernandavid4224@outlook.com>
2023-05-12 11:11:17 -0500

    agregar

--- main.tf
+++ main.tf
@@ -1,53 +1,22 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.28.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "rg_terraform"
-    storage_account_name = "terraformstorageutb"
-    container_name       = "tfstatesdevops"
-    key                  = "terraformgithub.tfstate"
+  backend "s3" {
+  bucket = "terraform-state-bucket-kio185"
+  key = "terraform.tfstate"
+  region = "us-east-1"
   }
 }
-
-provider "azurerm" {
-  features {
-  }
+provider "aws" {
+  region = "us-east-1"
 }
-
 variable "imagebuild" {
   type = string
   description = "the latest image build version"
 }
-
-resource "azurerm_resource_group" "rg_utbapp" {
-  name = "rg_utbapp" # this is the name on azure
-  location = "eastus" # data center location on azure
-}
-
-resource "azurerm_container_group" "tf_cg_utb" {
-  name                  = "utbapp"
-  location              = azurerm_resource_group.rg_utbapp.location #utilising the resource group
-  resource_group_name   = azurerm_resource_group.rg_utbapp.name #utilising the resource group
-
-  ip_address_type       = "Public"
-  dns_name_label        = "rafaelenrike-utb" #friendly name we want to give our domain
-  os_type               = "Linux"
-
-  # Specify the container information
-  container {
-    name = "utbapp"
-    image = "rafaelenrike/utbapp:${var.imagebuild}"
-    cpu = "1"
-    memory = "1"
-
-    ports {
-        port = 80
-        protocol = "TCP"
-    }
+resource "aws_ecr_repository" "devops-test-repo" {
+  name = "devops-test-repo"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+  scan_on_push = true
   }
+
 }
